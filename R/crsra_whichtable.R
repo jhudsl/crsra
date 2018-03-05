48ebf8d7c563ff57c08b5b061d7822f27ef1b744
ahdvnd <hadavand.a@gmail.com>
2018-03-04 19:20:39 -0500

    edits to the crsra_whichtable function

--- R/crsra_whichtable.R
+++ R/crsra_whichtable.R
@@ -3,10 +3,15 @@ crsra_whichtable <- function(x){
     if (exists("all_tables")==FALSE){
         stop("Please import the data first using crsra_import() command!")
     } else {
+        m <- 0
         for (i in 1:100){
             if (x %in% colnames(all_tables[[1]][[i]])){
                 print(tablenames[[i]])
+                m <- m + 1
             }
+        }
+        if (m==0){
+            print("There is no table that includes such variable. Please check the spelling!")
         }
     }
 }
