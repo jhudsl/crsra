af6c5a05af3c8d519008cc750702420ef7839b03
muschellij2 <muschellij2@gmail.com>
2018-03-06 14:58:16 -0500

    updated some memberhsharenames

--- R/crsra_import.R
+++ R/crsra_import.R
@@ -2,76 +2,36 @@
 #'
 #' @param workdir A character string vector indicating the directory where all the unzipped course directories are stored.
 #' @examples
-#' crsra_import()
-#' @export crsra_import
-
-crsra_import <- function(rmd = TRUE, workdir = getwd()) {
-
-    packages <- c("dplyr", "readr", "data.table", "reshape2", "reshape", "purrr", "plyr", "stringr", "tidytext", "rcorpora", "knitr")
-    instapack <- function(pkg){
-        new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-        if (length(new.pkg))
-            install.packages(new.pkg, dependencies = TRUE)
-        sapply(pkg, require, character.only = TRUE)
-    }
-    suppressMessages(suppressWarnings(instapack(packages)))
+#' zip_file = system.file("extdata", "fake_course_7051862327916.zip",
+#' package = "crsra")
+#' bn = basename(zip_file)
+#' bn = sub("[.]zip$", "", bn)
+#' res = unzip(zip_file, exdir = tempdir(), overwrite = TRUE)
+#' example_import = crsra_import(workdir = tempdir())
+#'
+#' @export
+crsra_import <- function(workdir = ".", ...) {
 
 
-    proceedstatus <- readline("The following procedure to import your data may take a while. Do you want to proceed? (y/n): ")
-    if (proceedstatus == "y") {
-        # This is for disconnecting all databases since there is a max of 16
-        workdir <- getwd()
-        dirnames <- list.dirs(path = workdir, full.names = FALSE)
-        dirnames <- dirnames[-1] # to remove the first empty folder name (check if this line will be an issue in windows)
-        #This is to check whether the working directory is pointing at the right folder.
-        dircheck <- list.files(file.path(workdir, dirnames), full.names = FALSE, recursive = TRUE, include.dirs = FALSE, pattern = "course_branch_grades.csv")
-        if (length(dircheck) == 0) {
-            stop("Please make sure you have set your working directory to where the Coursera data dump is located.")
-        }
-        numcourses <<- length(dirnames)
+    dn <- list.files(path = workdir,
+                     pattern = "course_branch_grades.csv",
+                     recursive = TRUE,
+                     full.names = TRUE)
+    dirnames = unique(dirname(dn))
 
-        ########################################################################
+    all_tables = purrr::map(dirnames, crsra_import_course, ...)
+    coursenames = sapply(all_tables, function(x){
+        attributes(x)$course_name
+    })
+    names(all_tables) = coursenames
 
-        # creates a list and populates it with each course and all the tables for each course
-        populate <- function(x, y) {
-            setwd(file.path(workdir, x))
-            names = list.files(pattern="*.csv")
-            tablenames <<- sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(names))
-            suppressMessages(suppressWarnings(all <- purrr::map(1:length(tablenames), ~ read_csv(names[.x]))))
-            # names(all) <- tablenames
-            # all[["peer_review_part_free_responses"]] <- as_data_frame(all[["peer_review_part_free_responses"]]) %>%
-            #    dplyr::select("peer_assignment_id", "peer_assignment_review_schema_part_id", "peer_review_id", "peer_review_part_free_response_text")
-        }
-        all_tables <<- purrr::map(1:numcourses, ~ populate(dirnames[[.x]]))
-        setwd(workdir)
+    partner_user_ids = sapply(all_tables, function(x){
+        attributes(x)$partner_user_id
+    })
+    partner_user_id = unique(partner_user_ids)
+    attr(all_tables, "partner_user_id") = partner_user_id
 
-        # this changes the table names and corrects the columns of the table "peer_review_part_free_responses"
-        for (i in 1:numcourses) {
-            names(all_tables[[i]]) <<- tablenames
-            all_tables[[i]][["peer_review_part_free_responses"]] <<- as_data_frame(all_tables[[i]][["peer_review_part_free_responses"]]) %>%
-                dplyr::select("peer_assignment_id", "peer_assignment_review_schema_part_id", "peer_review_id", "peer_review_part_free_response_text")
-        }
+    class(all_tables) = "coursera_import"
+    return(all_tables)
 
-        coursenames <<- purrr::map(1:numcourses, ~ all_tables[[.x]][["courses"]]$course_name) # Extracts course names, 85 is the number of table associated with table courses
-        names(all_tables) <<- coursenames # Assigns course names to the list courses
-        partner_user_id <<- colnames(all_tables[[1]][["users"]])[1]
-
-
-
-        # Since there are duplicates for membership roles (there are rows with the same jhu_user_id but different membership roles), the following lines
-        # will calculate the latest membership role and keep that for the jhu_user_id and delete all other rows.
-        # if (rmd == TRUE) {
-        #     for (i in 1:numcourses) {
-        #         all_tables[[i]][["course_memberships"]] <<- all_tables[[i]][["course_memberships"]] %>%
-        #             dplyr::filter(course_membership_role!="INSTRUCTOR") %>%
-        #             dplyr::filter(course_membership_role!="MENTOR") %>%
-        #             dplyr::group_by(jhu_user_id) %>%
-        #             dplyr::filter(course_membership_ts == max(course_membership_ts))
-        #     }
-        # } else {
-        #     message("Warning: There might be duplicate students since each student can take multiple roles")
-        # }
-        message(" The following courses are loaded:")
-        print(paste0(coursenames))
-    }
 }
--- R/crsra_membershares.R
+++ R/crsra_membershares.R
@@ -14,13 +14,26 @@
 #'   you lose some observations.
 #' @return A table which indicates the total number and the share of students in each group for each course
 #' @examples
-#' crsra_memebershares(groupby = "country")
+#' zip_file = system.file("extdata", "fake_course_7051862327916.zip",
+#' package = "crsra")
+#' bn = basename(zip_file)
+#' bn = sub("[.]zip$", "", bn)
+#' res = unzip(zip_file, exdir = tempdir(), overwrite = TRUE)
+#' example_import = crsra_import(workdir = tempdir())
+#' res = crsra_membershares(
+#' example_import,
+#' groupby = "country")
 #' @export
 crsra_membershares <- function(
+    all_tables,
     groupby = c("roles", "country", "language", "gender",
                 "empstatus", "education", "stustatus")) {
+    partner_user_id = attributes(all_tables)$partner_user_id
+
+    all_tables = course_to_coursera_import(all_tables)
 
     groupby = match.arg(groupby)
+    coursenames = names(all_tables)
     varname = switch(groupby,
                      "roles" = "course_membership_role",
                      "country" = "country_cd",
@@ -30,22 +43,24 @@ crsra_membershares <- function(
                      "education" = "educational_attainment",
                      "stustatus" = "student_status")
 
-     membershares <- function(x, z) {
-         temp <- z %>%
-             dplyr::left_join(x, by = partner_user_id, `copy`=TRUE)
-         temp$y = temp[[varname]]
-         temp %>%
-             dplyr::filter(!is.na(y)) %>%
-             dplyr::group_by(y) %>%
-             dplyr::summarise(Total=n()) %>%
-             dplyr::mutate(Share=round(Total/sum(Total),2)) %>%
-             dplyr::arrange(desc(Total))
-     }
-     membertable <- purrr::map(
-         1:numcourses, ~ membershares(all_tables[[.x]][["course_memberships"]],
-                                      all_tables[[.x]][["users"]]))
-     names(membertable) <- coursenames
-     return(membertable)
-     #' ggplot(temp, aes(course_membership_role)) + geom_bar(aes(weight = freq))
- }
+    membershares <- function(x, z) {
+        temp <- z %>%
+            dplyr::left_join(x, by = partner_user_id, `copy`=TRUE)
+        temp$y = temp[[varname]]
+        temp %>%
+            dplyr::filter(!is.na(y)) %>%
+            dplyr::group_by(y) %>%
+            dplyr::summarise(Total=n()) %>%
+            dplyr::mutate(Share=round(Total/sum(Total),2)) %>%
+            dplyr::arrange(desc(Total))
+    }
+    mems = lapply(all_tables, function(x) x$course_memberships)
+    users = lapply(all_tables, function(x) x$users)
+
+    membertable <- purrr::map2(
+        mems, users, membershares)
+
+    names(membertable) <- coursenames
+    return(membertable)
+}
 
